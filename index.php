1e623db0afbdfa531e329fc2fc177e282f16480a
o2ofever <samuel.yau@o2ofever.com>
2017-02-16 10:37:01 +0800

    Add files via upload
    Original XiaoZhuCMS index created by XiaoZhu.

--- index.php
+++ index.php
@@ -4,81 +4,8 @@ header("Content-type: text/html; charset=utf-8");
 ini_set('display_errors', '1');
 error_reporting(E_ALL ^ E_NOTICE);
 
+// echo "g = ".$_GET['g'];
 
-if ($_GET['a'] === 'price')
-	{
-		echo "<script>
-				window.open('http://www.o2ofever.com/price');
-				var win = window.open('http://wxoa.o2ofever.com/index.php?g=User&m=Function&a=show&id=6&token=qgsjlx1484101856', '_self');
-				win.focus();
-			  </script>";
-		die();
-		
-	}
-elseif  ($_GET['a'] === 'common')
-	{
-		echo "<script>
-				window.open('http://www.o2ofever.com/case-studies/power-888');
-				var win = window.open('http://wxoa.o2ofever.com/index.php?g=User&m=Function&a=show&id=6&token=qgsjlx1484101856', '_self');
-				win.focus();
-			  </script>";
-		die();
-		
-	}
-elseif  ($_GET['a'] === 'help')
-	{
-		echo "<script>
-				window.open('http://www.o2ofever.com/contact-us');
-				var win = window.open('http://wxoa.o2ofever.com/index.php?g=User&m=Function&a=show&id=6&token=qgsjlx1484101856', '_self');
-				win.focus();
-			  </script>";
-		die();
-		
-	}
-elseif  ($_GET['a'] === 'about2')
-	{
-		echo "<script>
-				
-				window.open('http://www.o2ofever.com/about-us');
-				var win = window.open('http://wxoa.o2ofever.com/index.php?g=User&m=Function&a=show&id=6&token=qgsjlx1484101856', '_self');
-				win.focus();
-			  </script>";
-		die();
-		
-	}
-elseif  ($_GET['a'] === 'fc')
-	{
-		echo "<script>
-				window.open('http://www.o2ofever.com/functionalities');
-				var win = window.open('http://wxoa.o2ofever.com/index.php?g=User&m=Function&a=show&id=6&token=qgsjlx1484101856', '_self');
-				win.focus();
-			  </script>";
-		die();
-		
-	}
-elseif  ($_GET['a'] === NULL)
-	{
-		echo "<script>
-				window.open('http://www.o2ofever.com/');
-				var win = window.open('http://wxoa.o2ofever.com/index.php?g=User&m=Function&a=show&id=6&token=qgsjlx1484101856', '_self');
-				win.focus();
-			  </script>";
-		die();
-		
-	}
-	
-	
-elseif  ($_GET['g'] == 'User' && $_GET['m'] == 'Index' && $_GET['a'] === 'index')
-	{
-		echo "<script>
-				window.open('http://wxoa.o2ofever.com/index.php?g=User&m=Function&a=show&id=6&token=qgsjlx1484101856', '_self');
-			  </script>";
-		die();
-		
-	}
-	
-	
-		
 if (get_magic_quotes_gpc()) {
 
  function stripslashes_deep($value){
@@ -92,8 +19,6 @@ if (get_magic_quotes_gpc()) {
  $_GET = array_map('stripslashes_deep', $_GET);
  $_COOKIE = array_map('stripslashes_deep', $_COOKIE);
 }
-
-	
 define('APP_NAME', 'cms');
 
 define('CONF_PATH','./Conf/');
@@ -115,76 +40,8 @@ if(!APP_DEBUG && is_file(RUNTIME_FILE)) {
     require RUNTIME_FILE;
 }
 else{
-//    echo "<html><head><link rel = 'stylesheet' type = 'text/css' href = 'https://bootswatch.com/united/bootstrap.min.css'></head></html>";
-	
-	defined('THINK_PATH') or define('THINK_PATH', dirname(__FILE__).'/');
+    defined('THINK_PATH') or define('THINK_PATH', dirname(__FILE__).'/');
 	
     require THINK_PATH.'Common/runtime.php';
-	
-	echo "
-			<link rel='stylesheet' type='text/css' href='https://bootswatch.com/cosmo/bootstrap.min.css'>
-			<link rel='stylesheet' type='text/css'  href='https://fonts.googleapis.com/icon?family=Material+Icons'>
-			<link rel='stylesheet' type='text/css' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
-			<script src='https://code.jquery.com/jquery-3.1.1.min.js'></script>
-			<script>
-					window.onload = function (){
-
-						$('.contentmanage').attr('style', 'width: auto');
-						$('.subCatalogList > a').attr('style', 'height: 50px'); $('.selected > a').attr('style', 'height: 50px');				
-						document.getElementsByTagName('a')[20].innerHTML = '<div style = \'float: left; margin-top: 5px;\' class=\"material-icons\">collections</div> <div style = \'float: left; margin-top: 5px;\'>图文组自动回复</div>';
-						document.getElementsByTagName('a')[1].innerHTML = '登出 Logout';
-						document.getElementsByTagName('a')[3].innerHTML = ' 技術支援 <i style = \'margin-top: 29px;\' class=\'fa fa-support\'></i>';
-						document.getElementsByTagName('a')[4].innerHTML = '設定 <i class = \'fa fa-cogs\'></i>';
-						document.getElementsByTagName('a')[5].innerHTML = '案例 <i class = \'fa fa-trophy\'></i>';
-						document.getElementsByTagName('a')[6].innerHTML = '价钱 <span class = \'glyphicon glyphicon-shopping-cart\'></span>';
-						document.getElementsByTagName('a')[7].innerHTML = '关于我们 <span class = \'fa fa-venus-mars\'></span>';
-						document.getElementsByTagName('a')[8].innerHTML = '功能 <span class = \'material-icons\' style = \'margin-top: 5px\'>widgets</span>';
-						document.getElementsByTagName('a')[16].innerHTML = '<span style = \'float: left; margin-top: -2px;\' class = \'fa fa-bar-chart\'></span> <div style = \'float: left; margin-top: 5px;\'>数据统计</div>';
-						document.getElementsByTagName('a')[17].innerHTML = '<div style = \'float: left; margin-top: 5px;\' class=\"material-icons\">mood</div> <div style = \'float: left; margin-top: 5px;\'>关注时自动回复</div>';
-						document.getElementsByTagName('a')[18].innerHTML = '<div style = \'float: left; margin-top: 5px;\' class = \'glyphicon glyphicon-send\'></div> <div style = \'float: left; margin-top: 5px;\'>纯文字自动回复</div>';
-						document.getElementsByTagName('a')[19].innerHTML = '<span style = \'float: left; margin-top: -2px;\' class = \'glyphicon glyphicon-picture\'></span><div style = \'float: left; margin-top: 5px;\'>图文自动回复</div>';
-						document.getElementsByTagName('a')[21].innerHTML = '<span style = \'float: left; margin-top: 5px;\' class=\'glyphicon glyphicon-headphones\'></span> <div style = \'float: left; margin-top: 5px;\'>语音回复</div>';
-						document.getElementsByTagName('a')[22].innerHTML = '<span style = \'float: left; margin-top: 5px;\' class=\'glyphicon glyphicon-bullhorn\'></span> <div style = \'float: left; margin-top: 5px;\'>群发</div>';
-						document.getElementsByTagName('a')[26].innerHTML = '<div style = \'float: left; margin-top: 5px;\' class=\"material-icons\">event_note</div> <div style = \'float: left; margin-top: 5px;\'>自定义菜单</div>';
-						$('.sideBar').attr('style','width: 230px !important');
-						$('.subCatalogList').css({'width':'210px'});
-						$('.tableContent').attr('style', 'width: auto');
-						$('.ckit').attr('style', 'padding-left: 10px');
-						
-
-					}
-					
-					
-					if ($('.feature_table').is(':visible')){
-							$('.feature_table').css('display', 'none');
-						var htSideBar = $('.sideBar').height();
-						var wd = $(window).width(); var wdSideBar = $('.sideBar').width(); var wdRight = wd - wdSideBar-7; var RcontainerBox = wdRight * 0.85;
-							
-							$('.container').css({'border':'solid 1px #c3c3c3', 'width': RcontainerBox+'px', 'height':'535px'});
-							$('.content').css({'float':'left', 'width': wdRight+'px', 'height': htSideBar+'px'});
-							$('.paging').css('display', 'none');
-					}
-					window.onresize = function(){
-						var wd = $(window).width();
-						var wdSideBar = $('.sideBar').width(); var wdRight = wd - wdSideBar-7;
-						$('.content').css({'float':'left', 'width': wdRight+'px'});
-					}
-					
-					$('.class_game').mouseover(function(){
-						$('.feature_table').css('display', 'block');
-						$('.container').attr('style', 'display: none !important');
-						$('.paging').attr('style', 'display: block !important');
-					});
-					
-					$('.class_game').mouseout(function(){
-						$('.feature_table').css('display', 'none');
-						var htSideBar = $('.sideBar').height();
-						var wd = $(window).width(); var wdSideBar = $('.sideBar').width(); var wdRight = wd - wdSideBar-7; var RcontainerBox = wdRight * 0.85;
-							
-							$('.container').css({'display':'block', 'border':'solid 1px #c3c3c3', 'width': RcontainerBox+'px', 'height':'535px'});
-							$('.content').css({'float':'left', 'width': wdRight+'px', 'height': htSideBar+'px'});
-							$('.paging').css('display', 'none');
-					});
-				</script>";
 }
 ?>
